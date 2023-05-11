d3c89ebd5c425e937fae486c1dbcad03e0e3c04f
Md Muhtasim Yasser <muhtasimyasser@gmail.com>
2023-05-11 16:44:06 -0400

    added index file and mocha+chai testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,18 @@
 const request = require('request');
-const breedName = process.argv.slice(2);
-const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(URL, (error, response, body) => {
-  if (error) {
-    console.log("Failed to request details: ", error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log(`Could not find ${breedName}`);
-  } else {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(URL, (error, response, body) => {
+    if (error) {
+      callback("Failed to request details: ", error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(`Could not find ${breedName}`, null);
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
+module.exports = { fetchBreedDescription };

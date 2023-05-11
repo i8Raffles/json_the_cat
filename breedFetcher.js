41ced30ba53f1db4ee46e76c4e8bcdb590f08483
Md Muhtasim Yasser <muhtasimyasser@gmail.com>
2023-05-11 16:23:53 -0400

    completed cats as a service assignment

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,16 @@
 const request = require('request');
+const breedName = process.argv.slice(2);
+const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+request(URL, (error, response, body) => {
+  if (error) {
+    console.log("Failed to request details: ", error);
+    return;
+  }
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log(`Could not find ${breedName}`);
+  } else {
+    console.log(data[0].description);
+  }
+});
